70d30f1dbfc2609a4c536ca5205fa9223e44d543
Jesse Millar <hellojessemillar@gmail.com>
2021-09-03 19:23:04 -0500

    Add files via upload

--- code.py
+++ code.py
@@ -3,42 +3,24 @@
 
 import board
 import digitalio
-import analogio
 import usb_hid
 
 from adafruit_hid.gamepad import Gamepad
 
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
 gp = Gamepad(usb_hid.devices)
 
-# Create some buttons. The physical buttons are connected
-# to ground on one side and these and these pins on the other.
-button_pins = (board.GP0, board.GP1, board.GP2, board.GP3)
-
-# Map the buttons to button numbers on the Gamepad.
-# gamepad_buttons[i] will send that button number when buttons[i]
-# is pushed.
-gamepad_buttons = (1, 2, 8, 15)
-
-buttons = [digitalio.DigitalInOut(pin) for pin in button_pins]
-for button in buttons:
-    button.direction = digitalio.Direction.INPUT
-    # button.pull = digitalio.Pull.UP
-
-# Connect an analog two-axis joystick to A4 and A5.
-#ax = analogio.AnalogIn(board.A4)
-#ay = analogio.AnalogIn(board.A5)
-
-# Equivalent of Arduino's map() function.
-#def range_map(x, in_min, in_max, out_min, out_max):
-#    return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
+pin1 = digitalio.DigitalInOut(board.GP1)
+pin1.switch_to_input(pull=digitalio.Pull.UP)
+#pin2 = digitalio.DigitalInOut(board.GP2)
 
 while True:
-    # Buttons are grounded when pressed (.value = False).
-    for i, button in enumerate(buttons):
-        gamepad_button_num = gamepad_buttons[i]
-        if button.value:
-            gp.release_buttons(gamepad_button_num)
-            #print(" release", gamepad_button_num, end="")
-        else:
-            gp.press_buttons(gamepad_button_num)
-            #print(" press", gamepad_button_num, end="")
+    led.value = not pin1.value
+
+    # Buttons are grounded when pressed (.value = False)
+    if not pin1.value:
+        gp.press_buttons(1)
+    else:
+        gp.release_buttons(1)
