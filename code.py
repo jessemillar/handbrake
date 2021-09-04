c9e27a7dcc8caa6a247ec1ca710e2cae66dfc86f
Jesse Millar <hellojessemillar@gmail.com>
2021-09-03 19:44:36 -0500

    Add files via upload

--- code.py
+++ code.py
@@ -15,9 +15,15 @@ pin1.switch_to_input(pull=digitalio.Pull.UP)
 pin2 = digitalio.DigitalInOut(board.GP2)
 pin2.switch_to_input(pull=digitalio.Pull.UP)
 
+pin3 = digitalio.DigitalInOut(board.GP3)
+pin3.switch_to_input(pull=digitalio.Pull.UP)
+
+pin4 = digitalio.DigitalInOut(board.GP4)
+pin4.switch_to_input(pull=digitalio.Pull.UP)
+
 while True:
     # Buttons are grounded when pressed (.value = False)
-    if not pin1.value or not pin2.value:
+    if not pin1.value or not pin2.value or not pin3.value or not pin4.value:
         led.value = True
     else:
         led.value = False
@@ -31,3 +37,13 @@ while True:
         gp.press_buttons(2)
     else:
         gp.release_buttons(2)
+
+    if not pin3.value:
+        gp.press_buttons(3)
+    else:
+        gp.release_buttons(3)
+
+    if not pin4.value:
+        gp.press_buttons(4)
+    else:
+        gp.release_buttons(4)
