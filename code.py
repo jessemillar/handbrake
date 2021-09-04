9bf48720ea3880e99852af2e7c4caa3aa5c6a5ef
Jesse Millar <hellojessemillar@gmail.com>
2021-09-03 19:25:57 -0500

    Add files via upload

--- code.py
+++ code.py
@@ -14,13 +14,23 @@ gp = Gamepad(usb_hid.devices)
 
 pin1 = digitalio.DigitalInOut(board.GP1)
 pin1.switch_to_input(pull=digitalio.Pull.UP)
-#pin2 = digitalio.DigitalInOut(board.GP2)
+
+pin2 = digitalio.DigitalInOut(board.GP2)
+pin2.switch_to_input(pull=digitalio.Pull.UP)
 
 while True:
-    led.value = not pin1.value
-
     # Buttons are grounded when pressed (.value = False)
+    if not pin1.value or not pin2.value:
+        led.value = True
+    else:
+        led.value = False
+
     if not pin1.value:
         gp.press_buttons(1)
     else:
         gp.release_buttons(1)
+
+    if not pin2.value:
+        gp.press_buttons(2)
+    else:
+        gp.release_buttons(2)
